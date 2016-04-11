4d3b9315b7a46e9e319a202456df30f7cfd3aa00
colejohnson <cjinak810@gmail.com>
2016-04-11 12:26:15 -0400

    Crunch time mo'trucka.

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,29 @@
 
 
 $(window).scroll(function(){
+
     if($(this).scrollTop() > 200){
         $('.scroll-down svg').css({'display': 'none'});
     }
+
+    if($(this).scrollTop() > 400){
+        $('#readmore1').fadeIn().css({'display': 'block'});
+    }
+
+
+    if($(this).scrollTop() > 1000){
+        $('#readmore2').fadeIn().css({'display': 'block'});
+    }
+
+    if($(this).scrollTop() > 1500){
+        $('#readmore3').fadeIn().css({'display': 'block'});
+    }
+
+    if($(this).scrollTop() <= 2000){
+        $('#readmore4').fadeIn().css({'display': 'block'});
+    }
+
+
+
+
 });
